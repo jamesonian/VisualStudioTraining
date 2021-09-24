38073623d3c44fc544422f8a53a0d0499a249434
jamesonian <33207551+jamesonian@users.noreply.github.com>
2021-09-24 10:35:46 -0700

    getting started

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;  // for Debug.WriteLine
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using ConsoleApp1.Classes;
+using StringLanguageExtensions;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        // BaseDirectory is the project bin folder and the Release or Debug subfolder
+        private static readonly string _fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Json", "PersonBirthdays.json");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Debug.WriteLine("Hello World!");
+            FindByFirstAndLastName( "John", "Smith");
+        }
+
+        private static void FindByFirstAndLastName(string fname, string lname)
+        {
+            if (File.Exists(_fileName))
+            {
+                
+            }
+            else
+            {
+                FileNotFound();
+            }
+
+        }
+
+        private static void FileNotFound()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Failed to find ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{ _fileName }");
+            Console.ResetColor();
+            Console.ReadLine();
         }
     }
 }
